173eacdfb356946e2628d7e629f28cde5198db3c
Renato Marques <renatomarques.contato@gmail.com>
2021-03-05 13:38:51 -0300

    Formulario de update junto com controller admin#update.

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -7,4 +7,18 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
     @admin = Admin.find(params[:id])
   end
 
+  def update
+    admin = Admin.find(params[:id])
+    if admin.update(params_admin)
+      redirect_to admins_backoffice_admins_path , notice: "Administrador atualizado com sucesso!"
+    else
+      render :edit
+    end
+  end
+
+  private
+  def params_admin
+    params.require(:admin).permit(:email, :password, :password_confirmation)
+  end
+
 end
