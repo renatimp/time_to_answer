9e7ab0604f692e6b7e02c318a4cf60aef6bc9bc3
Renato Marques <renatomarques.contato@gmail.com>
2021-03-08 22:00:48 +0000

    Criado edit e new para questoes

--- app/controllers/admins_backoffice/questions_controller.rb
+++ app/controllers/admins_backoffice/questions_controller.rb
@@ -1,9 +1,35 @@
 class AdminsBackoffice::QuestionsController < AdminsBackofficeController
 
-  before_action :set_question, only: [:destroy]
+  before_action :set_question, only: [:edit, :update, :destroy]
+  before_action :set_subject_options, only: [:new, :create, :edit, :update]
 
   def index
     @questions = Question.all.order(:description).page(params[:page])
+  end
+
+  def new
+    @question = Question.new
+    @subject = Subject.all
+  end
+
+  def create
+    @question = Question.new(params_question)
+    if @question.save
+      redirect_to admins_backoffice_questions_path, notice: "Pergunta cadastrada com sucesso!"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @question.update(params_question)
+      redirect_to admins_backoffice_questions_path, notice: "Pergunta atualizada com sucesso!"
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -15,7 +41,16 @@ class AdminsBackoffice::QuestionsController < AdminsBackofficeController
   end
 
   private
+  def params_question
+    params.require(:question).permit(:description, :subject_id)
+  end
+
   def set_question
     @question = Question.find(params[:id])
   end
+
+  def set_subject_options
+    @subject_type_options = Subject.all.pluck(:description, :id)
+  end
+
 end
