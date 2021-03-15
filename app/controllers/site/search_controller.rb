0408d98a211d4cea1d3300195b394f0bd8ae87ba
Renato Marques <renatomarques.contato@gmail.com>
2021-03-15 21:43:05 +0000

    Criado controller site::search e campo de pesquisa na index do site

---
+++ app/controllers/site/search_controller.rb
@@ -0,0 +1,8 @@
+class Site::SearchController < SiteController
+  def questions
+    @questions = Question.includes(:answers)
+                         .where("lower(description) LIKE ?",
+                                "%#{params[:term].downcase}%")
+                         .page(params[:page])
+  end
+end
