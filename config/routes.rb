4e4024da1a98ec0981b485ba559410e03a15abc4
Renato Marques <renatomarques.contato@gmail.com>
2021-03-19 20:15:42 +0000

    Desativado rota para sign_up admin e limpeza do layout user

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
     resources :subjects
     resources :questions
   end
-  devise_for :admins
+  devise_for :admins, skip: [:registrations]
   devise_for :users
   root to: 'site/welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
