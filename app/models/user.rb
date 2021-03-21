9d8ed8c39f90d996e112bd1ed2f86361cf33ad13
Renato Marques <renatomarques.contato@gmail.com>
2021-03-21 14:27:34 +0000

    Adicionado campos extras para usuarios

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # Assossiations
+  has_one :user_profile
+  accepts_nested_attributes_for :user_profile, reject_if: :all_blank
+
+  # Validations
   validates :first_name, presence: true, length: { minimum: 3 }
 
+  # Methods
   def full_name
     [self.first_name, self.last_name].join(' ')
   end
