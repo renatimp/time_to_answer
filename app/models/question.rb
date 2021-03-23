9022d80868ffbcd0e92c8216f7985f96024a7115
Renato Marques <renatomarques.contato@gmail.com>
2021-03-23 18:17:09 +0000

    Excluido paginaçao para quem nao tiver logado no sistema.

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,7 +5,7 @@ class Question < ApplicationRecord
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
 
   # Kaminari
-  paginates_per 10
+  paginates_per 5
 
   validates_presence_of :description
 
